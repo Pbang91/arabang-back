d73ea6cfaca1633734cf8fa8562f576f6187cc4b
Barney <holyja@naver.com>
2023-11-25 14:50:16 +0900

    feat: controller.spec 내용 삭제
    컨트롤러 테스트 코드 삭제
    - 현재 곧바로 service를 return 중
    - 별도 로직 발생 시 작성 예정
    - e2e 테스트 추후 진행

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,17 +1,1 @@
-import { PrismaService } from '../config/database/prisma.service';
-import { ItemsController } from './items.controller';
-import { ItemsService } from './items.service';
-
-describe('ItemsController', () => {
-  let itemsController: ItemsController;
-  let itemsService: ItemsService;
-
-  beforeEach(() => {
-    itemsService = new ItemsService(new PrismaService());
-    itemsController = new ItemsController(itemsService);
-  });
-
-  describe('createItem', () => {
-    it('생성된 업체 정보를 반환해야 합니다', async () => {});
-  });
-});
+// TODO: Controller에 별도 로직 들어갈 시 작성
